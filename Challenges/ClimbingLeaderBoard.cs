236a601f18098a21afe7cd60f6742e08aca35a24
Matthew Spindler <matthew.spindler@vistair.com>
2020-10-21 16:00:44 +0100

    Add CalculateRanks solution and tests

---
+++ Challenges/ClimbingLeaderBoard.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+
+namespace Challenges
+{
+    /*
+    An arcade game player wants to climb to the top of the leaderboard and track their ranking. The game uses Dense Ranking, so its leaderboard works like this:
+
+The player with the highest score is ranked number  on the leaderboard.
+Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
+Example
+
+
+
+The ranked players will have ranks , , , and , respectively. If the player's scores are ,  and , their rankings after each game are ,  and . Return .
+
+Function Description
+
+Complete the climbingLeaderboard function in the editor below.
+
+climbingLeaderboard has the following parameter(s):
+
+int ranked[n]: the leaderboard scores
+int player[m]: the player's scores
+Returns
+
+int[m]: the player's rank after each new score
+Input Format
+
+The first line contains an integer , the number of players on the leaderboard.
+The next line contains  space-separated integers , the leaderboard scores in decreasing order.
+The next line contains an integer, , the number games the player plays.
+The last line contains  space-separated integers , the game scores.
+
+Constraints
+
+ for 
+ for 
+The existing leaderboard, , is in descending order.
+The player's scores, , are in ascending order.
+Subtask
+
+For  of the maximum score:
+
+Sample Input 1
+
+CopyDownload
+Array: ranked
+100
+100
+50
+40
+40
+20
+10
+
+ 
+
+
+
+Array: player
+5
+25
+50
+120
+
+ 
+7
+100 100 50 40 40 20 10
+4
+5 25 50 120
+Sample Output 1
+
+6
+4
+2
+1
+    */
+
+    public class ClimbingLeaderBoard
+    {
+        /*
+        * Complete the 'climbingLeaderboard' function below.
+        *
+        * The function is expected to return an INTEGER_ARRAY.
+        * The function accepts following parameters:
+        *  1. INTEGER_ARRAY ranked
+        *  2. INTEGER_ARRAY player
+        */        
+        public static List<int> Calculate_slow(List<int> ranked, List<int> player)
+        {
+            List<int> results = new List<int>();
+
+            foreach(int playerScore in player)
+            {
+                int rank = 1;
+                int previousRankedPlayerScore = -1;
+                foreach(int rankedPlayerScore in ranked)
+                {
+                    if (playerScore >= rankedPlayerScore)
+                    {
+                       
+                        break;
+                    }
+
+                    if(previousRankedPlayerScore ==-1 || rankedPlayerScore != previousRankedPlayerScore)
+                        rank++;
+
+                    previousRankedPlayerScore = rankedPlayerScore;
+                }
+
+                 results.Add(rank);
+            }
+
+            return results;
+        }
+
+        public static List<int> Calculate_efficient(List<int> ranked, List<int> player)
+        {
+            List<int> results = new List<int>();
+
+            foreach(int playerScore in player)
+            {
+                int rank = 1;
+                int previousRankedPlayerScore = -1;
+                foreach(int rankedPlayerScore in ranked)
+                {
+                    if (playerScore >= rankedPlayerScore)
+                    {
+                       
+                        break;
+                    }
+
+                    if(previousRankedPlayerScore ==-1 || rankedPlayerScore != previousRankedPlayerScore)
+                        rank++;
+
+                    previousRankedPlayerScore = rankedPlayerScore;
+                }
+
+                 results.Add(rank);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ myTests/ClimbingLeaderBoardTests.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+using System.Collections;
+using System.Collections.Generic;
+
+using Challenges;
+
+namespace myTests
+{
+    public class TestDataCLBGenerator : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[] {new List<int>{100}, new List<int> {101}, new List<int> {1}},
+            new object[] {new List<int>{100}, new List<int> {90}, new List<int> {2}} ,
+            new object[] {new List<int>{100,90,90,80,75,60}, new List<int> {50,65,77,90,102}, new List<int> {6,5,4,2,1}}
+
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class ClimbingLeaderBoardTests
+    {
+        [Theory]
+        [ClassData(typeof(TestDataCLBGenerator))]
+        public void CalculateRanks_slow(List<int> ranked, List<int> player, List<int> result)
+        {
+            List<int> actualResult = ClimbingLeaderBoard.Calculate_slow(ranked, player);
+
+            Assert.Equal(result, actualResult);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestDataCLBGenerator))]
+        public void CalculateRanks_efficient(List<int> ranked, List<int> player, List<int> result)
+        {
+            List<int> actualResult = ClimbingLeaderBoard.Calculate_efficient(ranked, player);
+
+            Assert.Equal(result, actualResult);
+        }
+    }
+}
